76d958a60b4e712a7cb4de12dd0ea6f269b99c90
Gubbbb1215 <kukero2012@gmail.com>
2021-11-14 11:41:14 -0600

    Fixed ScheduleMaker

--- CometTrail/app/src/main/java/com/example/comettrail/ScheduleMaker.java
+++ CometTrail/app/src/main/java/com/example/comettrail/ScheduleMaker.java
@@ -15,15 +15,35 @@ public class ScheduleMaker {
         courses = myCourses;
         priority = myPriority;
         maxHours = mH;
+        allSchedules = new ArrayList<>();
+
+        // Insert code for making schedules
+        // Example course from myCourses: ECS1100
+        int curHours = 0;
+        int courseHours;
+        String curCourse;
+        Schedule mySchedule = new Schedule();
+
+        for (int i = 0; i < courses.size(); i++) {
+            curCourse = myCourses.get(i);
+            courseHours = curCourse.charAt(curCourse.length() - 2);
+            if (maxHours < curHours + courseHours) {
+                mySchedule.addCourse(curCourse);
+                curHours += courseHours;
+            }
+        }
+        this.allSchedules.add(mySchedule);
     }
 
     // Getters
     public ArrayList<String> getCourses() {return courses;}
     public ArrayList<Integer> getPriority() {return priority;}
     public int getMaxHours() {return maxHours;}
+    public ArrayList<Schedule> getAllSchedules() {return allSchedules;}
 
     // Setters
     public void setCourses(ArrayList<String> myArr) {courses = myArr;}
     public void setPriority(ArrayList<Integer> myArr) {priority = myArr;}
     public void setMaxHours(int mH) {maxHours = mH;}
+    public void setAllSchedules(ArrayList<Schedule> scheduleList) {allSchedules = scheduleList;}
 }
