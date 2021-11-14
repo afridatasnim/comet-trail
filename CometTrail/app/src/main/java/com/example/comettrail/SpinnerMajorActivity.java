7422017f669dcc1792c2112786cc3aa558f99642
ErikTheCleric <erikhale@outlook.com>
2021-11-14 10:14:09 -0600

    Updated UI with spinner views

---
+++ CometTrail/app/src/main/java/com/example/comettrail/SpinnerMajorActivity.java
@@ -0,0 +1,19 @@
+package com.example.comettrail;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class SpinnerMajorActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+
+	}
+}
